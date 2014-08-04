295922bedf3ebb082f85ac1b48fc69ca565406f6
Marion Itzel Martinez Ortega <Marion.itzel@hotmail.com>
2014-08-04 09:40:36 -0500

    Version1

---
+++ js/acciones.js
@@ -0,0 +1,18 @@
+//JavaScript
+	
+$(document).ready(function(e){
+	
+	$("#Beep").click(function(e){
+		
+	alert ('Le acabas de dar click al boton beep  ');
+	
+});//Click Beep
+	
+	$("#Vibrar").click(function(e){
+		
+	alert ('Le acabas de dar click al boton Vibrar  ');
+	
+});//Click Vibrar
+	
+	
+});//ready
