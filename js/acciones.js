31af059a9308517e3569a3bf9f97b4ca222ae91a
Marion Itzel Martinez Ortega <marion.itzel@hotmail.com>
2014-08-05 09:33:52 -0500

    Version 1.1.3

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,11 @@
 //JavaScript
-	
+	var audio ;
 $(document).ready(function(e){
+	
+	if( window.plugins && window.plugins.LowlatencAudio)
+	{audio=window.plugins.LowLatencAudio;audio.preloadFX('mario', 'audio/mario.mp3',function(msg){},function(msg){alert('error: '+msg);});
+	}//if
+	
 	
 	$("#Beep").click(function(e){
 		
@@ -13,6 +18,11 @@ $(document).ready(function(e){
 	navigator.notification.vibrate(1000);
 	
 });//Click Vibrar
+   
+	$("#Play").click(function(e){
+		
+	audio.play('mario');
 	
+});//Click Play
 	
 });//ready
