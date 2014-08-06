6ca560dbe4bc87bdd3f3bd9e633ccda5c5a12283
Marion Itzel Martinez Ortega <marion.itzel@hotmail.com>
2014-08-06 08:17:49 -0500

    Version 1.1.5

--- js/acciones.js
+++ js/acciones.js
@@ -2,27 +2,34 @@
 	var audio ;
 $(document).ready(function(e){
 	
-	if( window.plugins && window.plugins.LowlatencAudio)
-	{audio=window.plugins.LowLatencAudio;audio.preloadFX('mario', 'audio/mario.mp3',function(msg){},function(msg){alert('error: '+msg);});
-	}//if
+	document.addEventListener("deviceready",function(e){
 	
 	
-	$("#Beep").click(function(e){
+    
+	audio=window.plugins.LowLatencAudio;audio.preloadFX('mario', 'audio/mario.mp3',function(msg){},function(msg){alert('error: '+msg);});
+	
+	
+	
+	$('#Beep').bind("tap",function(e){
 		
 	navigator.notification.beep(1);;
 	
 });//Click Beep
 	
-	$("#Vibrar").click(function(e){
+	$("#Vibrar").bind("tap",function(e){
 		
 	navigator.notification.vibrate(1000);
 	
 });//Click Vibrar
    
-	$("#Play").click(function(e){
+	$("#Play").bind("tap",function(e){
 		
 	audio.play('mario');
 	
 });//Click Play
+
+
+
+	});//deviceready
 	
 });//ready
